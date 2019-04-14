ca3a82e337503124bb16a434f13d7e0dcb714a87
Leandro <leandroercoli@gmail.com>
2019-04-14 13:42:10 -0300

    v2.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d34fa87a65614ba84a1f956e927eede.js"
+  "/precache-manifest.a7f2dbc2e75bf89eb2537bad39445170.js"
 );
 
 workbox.clientsClaim();
