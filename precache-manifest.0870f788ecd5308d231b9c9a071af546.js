89001ce8862708d1ed1056338eb5a5ba55bd9214
Leandro <leandroercoli@gmail.com>
2019-10-27 14:01:03 -0300

    actualizacion nuevo formato

---
+++ precache-manifest.0870f788ecd5308d231b9c9a071af546.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeb37c1f4decb0c9352355880bd5843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21bda390a14864666738",
+    "url": "/static/css/main.41de191c.chunk.css"
+  },
+  {
+    "revision": "ea28601a662702a0759a",
+    "url": "/static/js/2.c054c244.chunk.js"
+  },
+  {
+    "revision": "21bda390a14864666738",
+    "url": "/static/js/main.ff634a85.chunk.js"
+  },
+  {
+    "revision": "36d9bdb8378776ca7823",
+    "url": "/static/js/runtime-main.bb84221d.js"
+  },
+  {
+    "revision": "2ec9d460cfa410dd2046677b98873b7e",
+    "url": "/static/media/IMG-20170409-WA0023-round.2ec9d460.jpg"
+  },
+  {
+    "revision": "74c63ad2c9d0aa1c3909cf5c956277a5",
+    "url": "/static/media/banner-plantcare.74c63ad2.png"
+  },
+  {
+    "revision": "ea336b8608af0e9a1b0d1a765e2b5015",
+    "url": "/static/media/baselines.ea336b86.jpg"
+  },
+  {
+    "revision": "908e84de572a9528ed6e7ac7c8b6edaf",
+    "url": "/static/media/icon-medium.908e84de.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d60fa2cbe1d779ee3d5885f261560c8e",
+    "url": "/static/media/smarthome-interface.d60fa2cb.jpg"
+  },
+  {
+    "revision": "6148313603c464e101a3faba1c6955fc",
+    "url": "/static/media/space-game.61483136.jpg"
+  },
+  {
+    "revision": "908c46185567d3302b1c83fc9bea9478",
+    "url": "/static/media/stock-predictor.908c4618.jpg"
+  },
+  {
+    "revision": "0f216b1165c55d2b5b7dd76a0b886d8e",
+    "url": "/static/media/tutorialesuns-interface.0f216b11.jpg"
+  },
+  {
+    "revision": "d598e0f79935b8a46b76122e6e204a6d",
+    "url": "/static/media/web-stockpredictor.d598e0f7.jpg"
+  }
+]);
